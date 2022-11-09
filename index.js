2f985c8b3a8898aedb6a6dc742ee1e91cf553074
FmHasibul <fmhhasan@gmail.com>
2022-11-09 13:54:14 +0800

    data added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const foods = require('./food.json')
 
 const port = process.env.PORT || 5000;
 
@@ -10,6 +11,10 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('pastry wala server is ruuning')
 });
+
+app.get('/foods', (req, res) => {
+    res.send(foods)
+})
 
 app.listen(port, () => {
     console.log(`server is running on port = ${port}`)
